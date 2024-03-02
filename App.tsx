7eff82bd235ab9e106c7c1c6d6b07235e58721e0
Hossein Hashemi <hossein.hashhemi.com>
2024-03-03 01:58:13 +0330

    adding main redux state and its interfaces, adding react navigation provider, making new stack and registering empty components

--- App.tsx
+++ App.tsx
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Platform, SafeAreaView, StatusBar, StyleSheet } from "react-native";
+import { Provider } from "react-redux";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import store from "./src/services/store";
+
+import Home from "./src/pages/home";
+import AddTask from "./src/pages/addTask";
+import EditTask from "./src/pages/editTask";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        paddingTop: Platform.OS == "android" ? StatusBar.currentHeight : 0,
+      }}
+    >
+      <Provider store={store}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              options={{ headerShown: false }}
+              name="Home"
+              component={Home}
+            />
+            <Stack.Screen
+              options={{ headerShown: false }}
+              name="AddTask"
+              component={AddTask}
+            />
+            <Stack.Screen
+              options={{ headerShown: false }}
+              name="EditTask"
+              component={EditTask}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Provider>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
