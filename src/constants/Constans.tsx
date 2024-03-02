292f9736c1a27f33f32efb304b8ffc6d3b880439
Hossein Hashemi <hossein.hashhemi.com>
2024-03-03 02:01:59 +0330

    adding constants.tsx for main constants values

---
+++ src/constants/Constans.tsx
@@ -0,0 +1,17 @@
+import { Dimensions } from "react-native";
+
+const WIDTH = Dimensions.get("window").width;
+const HEIGHT = Dimensions.get("window").height;
+const mainContainersPadding = 20;
+const mainContainersPaddingTop = 50;
+const buttonHeight = 40;
+const mainColor = "rgb(69, 88, 179)";
+
+export {
+  WIDTH,
+  HEIGHT,
+  mainContainersPadding,
+  buttonHeight,
+  mainColor,
+  mainContainersPaddingTop,
+};
